734aec9bbadd26f79f7b34735bc12fca9a036086
paul-karas-nws <pkaras@normandiewebschool.fr>
2025-03-14 11:49:47 +0100

    Navigation-config
    -ajout de lien, titre et description des liens header dans le dossier config.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
 export const siteConfig = {
-    bannerMessage: "⚠️ Le marché floral 2025 est Annulé", 
-  };
+  bannerMessage: "⚠️ Le marché floral 2025 est Annulé",
+  mainMenu: [
+    { url: "/about", title: "À propos", description: "Programme et animation" },
+    { url: "/exposants", title: "Exposants", description: "Des professionnels à l'écoute" },
+    { url: "/infos", title: "Infos Pratiques", description: "Restauration, hébergement,..." },
+  ],
+};
