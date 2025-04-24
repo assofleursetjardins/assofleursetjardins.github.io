7daa34ed8496287422ab08bcb70dad7266593c57
Raphael Sanchez <hello@raphaelsanchez.design>
2025-04-24 17:39:00 +0200

    Enhance Astro configuration to include environment variable for PUBLIC_SITE_URL and update site property accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { loadEnv } from 'vite';
 
 import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
 
+const { PUBLIC_SITE_URL } = loadEnv(
+  process.env.NODE_ENV || 'development',
+  process.cwd(),
+  '',
+);
+
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_SITE_URL: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+    },
+  },
+  site: PUBLIC_SITE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
